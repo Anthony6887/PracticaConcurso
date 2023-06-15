a38f36db602fe000445883eef81d1dd6ef9cfbd2
Anthony S <asolis7378@uta.edu.ec>
2023-06-15 18:48:39 -0500

    Se agrefo el insertarPersona

--- persona.php
+++ persona.php
@@ -1,13 +1,30 @@
 <?php
 include_once "conexion.php";
 
+class Persona
+{
+public function obtenerPersonas(){
 $con = Conexion::conectar();
 $sql = "SELECT * FROM personas";
 $query = $con->prepare($sql);
 $query->execute();
 $result = $query->fetchAll();
 echo json_encode($result);
+}
 
+public function agregarPersona(){
+$con = Conexion::conectar();
+$cedula = $_POST['cedula'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$estado = "activo";
+$edad = $_POST['edad'];
+$consulta = "INSERT INTO personas (cedula,nombre,apellido,edad,estado)values('$cedula','$nombre','$apellido','$edad','$estado')";
+$resultado = $con->prepare($consulta);
+$resultado->execute();
+$dato = "Se inserto correctamente";
+echo json_encode($dato);
 
-
+}
+}
 ?>
